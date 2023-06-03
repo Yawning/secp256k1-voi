afa636b3aa035074ca6a389b5596d9eefe48fbc7
Yawning Angel <yawning@schwanenlied.me>
2023-06-03 20:29:37 +0900

    secp256k1: FOSS
    Fixes #1

--- point_mul_table_amd64.go
+++ point_mul_table_amd64.go
@@ -1,11 +1,6 @@
-// Copyright 2023 Yawning Angel.  All Rights Reserved.
+// Copyright (c) 2023 Yawning Angel
 //
-// secp256k1-voi can be used in non-commercial projects of any kind,
-// excluding those relating to or containing non-fungible tokens
-// ("NFT") or blockchain-related projects.
-//
-// The package can not be modified to suit your needs. You may not
-// redistribute or resell it, even if modified.
+// SPDX-License-Identifier: BSD-3-Clause
 
 //go:build amd64 && !purego
 
